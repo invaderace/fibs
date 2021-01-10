bf58efcf40abe870935b9fb5d7f0d28474bacb53
Michael Mattson <michael.p.mattson@gmail.com>
2021-01-10 14:15:30 -0700

    added better recursive solution.

---
+++ lib/fibs_recursive.rb
@@ -0,0 +1,18 @@
+def fibs(num)
+  if num < 2
+    num
+  else
+    num = fibs(num-2) + fibs(num-1)
+  end
+end
+
+puts "Fibs(0): #{fibs(0)}"
+puts "Fibs(1): #{fibs(1)}"
+puts "Fibs(2): #{fibs(2)}"
+puts "Fibs(3): #{fibs(3)}"
+puts "Fibs(4): #{fibs(4)}"
+puts "Fibs(5): #{fibs(5)}"
+puts "Fibs(6): #{fibs(6)}"
+puts "Fibs(7): #{fibs(7)}"
+puts "Fibs(8): #{fibs(8)}"
+puts "Fibs(9): #{fibs(9)}"
